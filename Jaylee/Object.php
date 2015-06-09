a5fd4e8c4c2e193d7af452c6ad38c21be4e80576
jaylee <sjclijie@vip.qq.com>
2015-06-10 01:20:02 +0800

    add Object

---
+++ Jaylee/Object.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaylee
+ * Date: 15/6/10
+ * Time: 01:06
+ */
+
+namespace Jaylee;
+
+class Object {
+
+    public function __get($name){
+
+        $getter =  "get" . $name;
+        $setter =  "set" . $name;
+
+        if (method_exists($this, $getter)){
+
+            return $this->$getter();
+
+        }else if (method_exists($this, $setter)){
+
+            throw new \Exception("Getting write-only property: " . get_class($this) . "::" . $name);
+
+        }else {
+
+            throw new \Exception("Getting unknown property: " . get_class($this) . "::" . $name);
+        }
+    }
+
+    public function __set($name, $value){
+
+        $getter = "get" . $name;
+        $setter = "set" . $name;
+
+        if (method_exists($this, $setter)){
+
+            $this->$setter = $value;
+
+        }else if (method_exists($this, $getter)){
+
+            throw new \Exception("Setting read-only property: " . get_class($this) . "::" . $name);
+
+        }else {
+
+            throw new \Exception("Setting unknown property: " . get_class($this) . "::" . $name);
+        }
+    }
+}
